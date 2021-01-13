ea1f9485bd8f94e0a72371f3be45c6c1f9b359a6
Mark Rawls <markrawls96@gmail.com>
2021-01-12 20:03:59 -0500

    Functional prototype

--- pdf_image_adding/cli.py
+++ pdf_image_adding/cli.py
@@ -1,14 +1,35 @@
 import argparse
 import sys
 
+from fitz import Document, Pixmap, Rect
+
 
 def main(args):
-    print(args.file_name)
+    with open(args.pdf_file_name, "rb") as f:
+        pdf_data = Document(stream=f.read(), filetype="pdf")
+
+    with open(args.img_file_name, "rb") as f:
+        pixmap = Pixmap(f.read())
+
+    position = Rect(args.x, args.y, args.x + pixmap.width, args.y + pixmap.height)
+    page = pdf_data[args.page]
+
+    page.insertImage(position, pixmap=pixmap)
+
+    with open(args.o, "wb") as f:
+        f.write(pdf_data.write())
 
 
 def cli():
     parser = argparse.ArgumentParser()
-    parser.add_argument("file_name")
+    parser.add_argument("pdf_file_name")
+    parser.add_argument("img_file_name")
+
+    parser.add_argument("-x", type=int, default=0)
+    parser.add_argument("-y", type=int, default=0)
+    parser.add_argument("-o", default="out.pdf")
+    parser.add_argument("-p", "--page", type=int, default=0)
+
     args = parser.parse_args()
 
     sys.exit(main(args))
